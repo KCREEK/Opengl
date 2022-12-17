fdee2b47447abf75b0c75aa71b7452bfc3bcdba0
Siddhant Rakibe <102241647+KCREEK@users.noreply.github.com>
2022-12-18 01:10:27 +0530

    Drawing sunrise and sunset using opengl

---
+++ cgpractical/sunrise.cpp
@@ -0,0 +1,18 @@
+//including the header files
+//Program for drawing sunrise and sunset
+#include <iostream>
+#include <stdlib.h>
+
+ifdef_APPLE_
+#include <openGL/openGL.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+using namespace std;
+
+
+int main(){
+    
+}
