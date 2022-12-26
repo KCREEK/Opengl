82331c6fa3d27a668b805e279cb11ff0fffa4a27
Siddhant Rakibe <102241647+KCREEK@users.noreply.github.com>
2022-12-26 19:25:29 +0530

    `kbhit` and `getch` application

---
+++ cgpractical/bouncingball.cpp
@@ -0,0 +1,16 @@
+//write a cpp program to implement bouncing ball using sine-wave form. Apply the concept of polymorphism
+
+#include <iostream>
+#include <graphics.h>
+#include <conio.h>
+#include <dos.h>
+
+int main(){
+    int gd=0,gm,x=20,flag=0,y=200,uplimit=250;
+    initgraph(&gd,&gm,"C\\Tc\\BGI");
+    while(!kbhit()){
+        //what kbhit does is that it iterates the statement until you press a key
+        //later getch returns the key stroke
+        
+    }
+}
